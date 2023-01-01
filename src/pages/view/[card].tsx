62b7ef3cb97ee1594e19f74f327eec739b756f8a
fe-i <feic47@gmail.com>
2022-12-31 20:56:00 -0500

    add FIREWORKS!

--- src/pages/view/[card].tsx
+++ src/pages/view/[card].tsx
@@ -1,8 +1,10 @@
 import { Flex, Text, Spinner, Button, useClipboard } from "@chakra-ui/react";
 import { DocumentData } from "@firebase/firestore";
+import Fireworks from "@fireworks-js/react";
+import type { FireworksHandlers } from "@fireworks-js/react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Card from "../../components/card";
 import Layout from "../../components/layout";
 import useFirebase from "../../hooks/useFirebase";
@@ -18,9 +20,34 @@ const ViewCard: NextPage = () => {
 		setValue(window.location.toString());
 	}, [query]);
 
+	const ref = useRef<FireworksHandlers>(null);
+
+	const toggle = () => {
+		if (!ref.current) return;
+		if (ref.current.isRunning) {
+			ref.current.stop();
+		} else {
+			ref.current.start();
+		}
+	};
+
 	return (
 		<Layout title="View Card">
+			<Flex pointerEvents="none">
+				<Fireworks
+					ref={ref}
+					options={{ opacity: 0.2, traceSpeed: 5, intensity: 40 }}
+					style={{
+						top: 0,
+						left: 0,
+						width: "100%",
+						height: "100%",
+						position: "fixed"
+					}}
+				/>
+			</Flex>
 			<Flex flexDir="column" align="center" justify="center" fontSize="lg" gap={5} py={8}>
+				<Button onClick={toggle}>Toggle Fireworks</Button>
 				{card ? (
 					<>
 						<Card
